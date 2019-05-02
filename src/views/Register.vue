73e176cb09e50208c177c00a3478c73c51bc3d05
Oleg <ooleg-1999@list.ru>
2019-05-02 22:24:42 +0300

    register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,35 +1,56 @@
 <template>
 	<div class="register">
-		<transition id="register" mode="out-in">
-			<register></register>
-		</transition>
+		<form action="" onsubmit="return false">
+			<transition name="register" mode="out-in">
+				<component :is="view"></component>
+			</transition>
+			<!-- <button id="phone_submit">Далее</button> -->
+		</form>
 	</div>
 </template>
 
 <script>
-	export default({
-		el: 'register',
+	export default{
 		data: function() {
-return {
+	return {
 			view: 'phone'
-		};
-},
+		}
+	},
 		components: {
 			'phone': {
-				template: '<input type="number">'
+				template: '<div class="input_group"><label for="phone">Номер телефона</label><input id="phone" name="phone" type="text"><button id="phone_submit">Далее</button></div>'
 			},
-			'password': {
-				template: ''
+			'sms_code': {
+				template: '<div class="input_group"><label for="sms_code">Смс код</label><input id="sms_code" name="sms_code" type="text"></div>'
 			},
-			'other-info': {
-				template: ''
+			'other_info': {
+				template: '<div class="input_group"><label for="first_name">Имя</label><input id="first_name" name="first_name" type="text"></div><div class="input_group"><label for="last_name">Фамилия</label><input id="last_name" name="last_name" type="text"></div><div class="input_group"><label for="city">Город</label><input id="city" name="city" type="text"></div>'
 			}
-		}
-	});
+		},
+		methods: {
+			click: function(view) {
+				this.button.onclick = function(){
+					alert('help');
+				}
+			},
+		},
+	};
 </script>
 
-<style>
-	#register {
+<style scope>
+	.register {
 		margin-top: 100px;
+	}
+	.input_group label {
+		display: inline-block;
+	}
+	.input_group input {
+		display: block;
+	}
+	.component-fade-enter-active, .component-fade-leave-active {
+		transition: opacity .3s ease;
+	}
+	.component-fade-enter, .component-fade-leave-to{
+		opacity: 0;
 	}
 </style>
